57d27572955d759b0e38f9ac063834bda8b08b17
shivrambihari15 <86231673+shivrambihari15@users.noreply.github.com>
2022-11-19 19:00:24 +0530

    Add files via upload

---
+++ kbhere_2/src/components/Middle.js
@@ -0,0 +1,21 @@
+import React from "react";
+import robo from "../css/images/robo.png";
+import "./middle.css";
+const Middle = () => {
+  return (
+    // <div
+    //   className="h-100 wrapper_by_middle"
+    //   style={{
+    //     maxHeight: "100%",
+    //     minWidth: "auto",
+    //     minHeight: "70%",
+    //     display: "flex",
+    //   }}
+    // >
+    //   <img src={robo} alt="Image" />
+    // </div>
+    <></>
+  );
+};
+
+export default Middle;
---
+++ kbhere_2/src/pages/Index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "../App.css";
+import Header from "../components/Header";
+import Middle from "../components/Middle";
+import Footer from "../components/Footer";
+const Index = () => {
+  return (
+    <div className="main-wrap">
+      <Header />
+    </div>
+  );
+};
+
+export default Index;
